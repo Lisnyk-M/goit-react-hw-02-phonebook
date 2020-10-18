e0dddac1d885a02bea3c501982ecd6adb9665bd3
Lisnyk-M <swapbuffers@gmail.com>
2020-10-18 11:09:22 +0300

    Updates

---
+++ precache-manifest.29edb68e9c0f68c775ccc32705fcd9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0557cf98f7d17ac40b10ca29459bc7ee",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "4753e9ca6ad848127a54",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.dba306ed.chunk.css"
+  },
+  {
+    "revision": "21060f0f9efa570474ec",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3bbdf382.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3bbdf382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4753e9ca6ad848127a54",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.a49bc663.chunk.js"
+  },
+  {
+    "revision": "21ee8420ea4ed4da3dd0",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.dfaa9ef1.js"
+  }
+]);
